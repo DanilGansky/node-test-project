4c8014b48bacecc5c0c2017666357a839d253fad
danilgansky <danylo.hanskyi@computools.com>
2021-03-15 10:55:55 +0200

    integration tests for identity api

---
+++ app/identity/__tests__/identityAPI.js
@@ -0,0 +1,41 @@
+const request = require("supertest");
+const app = require("../../../server");
+const db = require("../db");
+
+const testEmail = "test@mail.test.com";
+const testPassword = "12345678";
+const testPhoneNumber = "+380962582171";
+
+afterAll(async () => {
+  try {
+    await db.ActivationCode.destroy({ truncate: true });
+    await db.ActivationToken.destroy({ truncate: true });
+    await db.AccessToken.destroy({ truncate: true });
+    await db.User.destroy({ truncate: true, cascade: true });
+  } finally {
+    await db.sequelize.close();
+  }
+});
+
+describe("testing identity API (integration tests)", () => {
+  test("register", async () => {
+    const resp = await request(app).post("/identity/register").send({
+      email: testEmail,
+      password: testPassword,
+    });
+
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body.user.email).toEqual(testEmail);
+  });
+
+  test("login", async () => {
+    const resp = await request(app).post("/identity/login").send({
+      email: testEmail,
+      password: testPassword,
+      phoneNumber: testPhoneNumber,
+    });
+
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body.result).not.toBeUndefined();
+  });
+});
--- index.js
+++ index.js
@@ -1,54 +1,5 @@
-require("dotenv").config();
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const { identityAPI, publicAPI } = require("./app/identity");
-const {
-  characterAPI,
-  adminCharacterAPI,
-  adminSkillAPI,
-  adminItemAPI,
-} = require("./app/character");
-
-const fs = require("fs");
+const app = require("./server");
 const { appConfig } = require("./app/config");
-const { authMiddleware, isAdminMiddleware } = require("./app/middlewares");
-
-const app = express();
-const corsOptions = {
-  origin: `http://${appConfig.HOST}:${appConfig.PORT}`,
-};
-
-// Middlewares
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-
-// Routers
-app.use("/identity", identityAPI, publicAPI);
-app.use(
-  "/admin",
-  authMiddleware,
-  isAdminMiddleware,
-  adminCharacterAPI,
-  adminSkillAPI,
-  adminItemAPI
-);
-
-app.use("/character", authMiddleware, characterAPI);
-
-app.use("/media", (req, resp) => {
-  fs.readFile(__dirname + "/media/" + req.url, (err, text) => {
-    if (err) {
-      resp.status(404);
-      resp.end(JSON.stringify(err));
-      console.log(err);
-    }
-
-    resp.status(200);
-    resp.end(text);
-  });
-});
 
 app.listen(appConfig.PORT, () => {
   console.log(`Running on ${appConfig.HOST}:${appConfig.PORT}...`);
