9c61943f8aa1651822ed15d5fbba058c5dba7f19
danilgansky <danylo.hanskyi@computools.com>
2021-03-18 11:44:24 +0200

    updated index.js, publicAPI.js

--- app/identity/publicAPI.js
+++ app/identity/publicAPI.js
@@ -1,13 +1,5 @@
 const express = require("express");
-const { appConfig } = require("../config");
-
-let controller;
-
-// todo: make mocks
-if (appConfig.TEST) {
-} else {
-  controller = require("./publicController");
-}
+const controller = require("./publicController");
 
 const router = express.Router();
 
--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,12 +1,4 @@
-const { appConfig } = require("../config");
-
-let repository;
-
-// todo: make mocks
-if (appConfig.TEST) {
-} else {
-  repository = require("../identity/activation/accessTokenRepository");
-}
+const repository = require("../identity/activation/accessTokenRepository");
 
 module.exports = {
   authMiddleware: require("./authMiddleware")(repository),
